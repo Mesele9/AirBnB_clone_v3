92fd12abe929f014a29a0ea1f5fd022596740277
Mesele <getmesy@gmail.com>
2023-04-30 02:09:13 +0000

    update files

--- api/v1/app.py
+++ api/v1/app.py
@@ -7,12 +7,15 @@ from os import getenv
 
 
 app = Flask(__name__)
+app.config['JSONIFY_PRETTYPRINT_REGULAR'] = True
 app.register_blueprint(app_views)
+
 
 @app.teardown_appcontext
 def close_db(obj):
     """a method that calll storage.close()"""
     storage.close()
+
 
 if __name__ == '__main__':
     host = getenv('HBNB_API_HOST', default='0.0.0.0')
--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -4,6 +4,7 @@ from flask import Flask
 from api.v1.views import app_views
 from flask import jsonify
 
+
 @app_views.route('/status', strict_slashes=False)
 def status():
     return jsonify({"status": "OK"})
